50e8cb96a29cde7cdfafd794da977927a08172a3
Vimal Rampaul <vrampaul@icloud.com>
2020-06-15 23:35:27 -0400

    update

--- js/logic.js
+++ js/logic.js
@@ -35,24 +35,3 @@ function toggleFunction() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
-
-//Google Map API
-function myMap() {
-    myCenter = new google.maps.LatLng(40.7831, -73.9712);
-    var mapOptions = {
-      center: myCenter,
-      zoom: 11,
-      scrollwheel: false,
-      draggable: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(
-      document.getElementById("googleMap"),
-      mapOptions
-    );
-  
-    var marker = new google.maps.Marker({
-      position: myCenter
-    });
-    marker.setMap(map);
-  }
